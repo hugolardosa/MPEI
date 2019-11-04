06a4acf58524ee033cf7ed3f755cc47a26c2bcdf
LuisaTheAmaral <luisa.amaral9@gmail.com>
2019-11-04 09:29:20 +0000

    Adição dos guiões

--- Pratica4/Ex8.m
+++ Pratica4/Ex8.m
@@ -1,14 +1,16 @@
 %% Exercício 8
 %considerar a variância 7?
-sr = round(sqrt(10)*BoxMuller(30)+0); %%perguntar ao prof se podemos fazer round
+var = 2;
+media = 14;
+sr = sqrt(var)*BoxMuller(30)+media; %%perguntar ao prof se podemos fazer round
 sr
 subplot(1,2,1)
-histogram(sr,-7:1:7)
+histogram(sr,0:1:20)
 %axis([0 20 0 20])
 title('Sem rejeição')
-sr(sr<-5 | sr>5) = [];
+sr(sr< -5*var+media | sr>5*var+media) = [];  % aplicar o método da gaussiana normalizada
 subplot(1,2,2)
-histogram(sr,-7:1:7)
+histogram(sr,0:1:20)
 title('Com rejeição')
 %axis([0 20 0 20])
 max = sum(sr == mode(sr));
