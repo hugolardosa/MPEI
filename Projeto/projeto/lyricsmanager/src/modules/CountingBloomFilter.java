79fd784b1b736faac4ba5ff4ac9d6a5839f5200b
Hmll <hugolardosa@gmail.com>
2019-11-18 17:47:24 +0000

    Inicio do BloomFilter e base de dados

--- Projeto/projeto/lyricsmanager/src/modules/CountingBloomFilter.java
+++ Projeto/projeto/lyricsmanager/src/modules/CountingBloomFilter.java
@@ -1,8 +1,58 @@
+//MPEI- 2019 - UA
+//Projeto Final
+//Luísa Amaral (93001)
+//Hugo Leal (93059)
 package modules;
 
 public class CountingBloomFilter {
+	private int n; //Numero de bits do filtro
+	private int m; //numero de elementos de um conjunto
+	private int k; //numero de funções de dispersão
+	private int BloomVector[] ; //bloom vector 
+	private HashFunction h = new HashFunction();
 	
-	public CountingBloomFilter() {
+	//contrutores
+	public CountingBloomFilter(int m,double fatordecarga) {
+		
+		this.m=m;
+		this.n= (int) Math.round(m/fatordecarga);
+		this.k=(int) Math.floor((n*Math.round(m/fatordecarga)));
+		BloomVector = new int[k];
+	}
+	
+	public CountingBloomFilter(double fatordecarga,int m,int k) {
+		this.k=k;
+		this.m=m;
+		this.n= (int) Math.round(m/fatordecarga);
+		BloomVector = new int[k];
+	}
+	
+	public CountingBloomFilter(int m,int k, double fatordecarga) {
+		this.k=k;
+		this.m=m;
+		this.n=n;
+	}
+	
+
+	
+	//inicialização
+	public void init(int k) {
+		if(BloomVector == null)BloomVector = new int[k];
+		for (int i = 0; i < BloomVector.length; i++) {
+			BloomVector[i]=0;
+		}
+	}
+	
+	//adicionaElemento
+	public void addElement(String Element) {
+		for (int i = 0; i < k; i++) {
+			int hash = (int) h.hash(Element + i); //Desenvolver hash(String) HashFunction no 
+			BloomVector[hash]++; 
+		}
+	}
+	
+	//verfica se é membro
+	public void membro() {
 		
 	}
 }
