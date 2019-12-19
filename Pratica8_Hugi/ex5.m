82aca70eb5ee84df03349effd1888c91846ff673
hugolardosa <45799046+hugolardosa@users.noreply.github.com>
2019-12-19 11:22:50 +0000

    ex5

--- Pratica8_Hugi/ex5.m
+++ Pratica8_Hugi/ex5.m
@@ -1,7 +1,53 @@
 %% alinea a 
-T = ([0.7 0.2 0.1;0.2 0.3 0.5;0.3 0.3 0.4])'
+T = ([0.7 0.2 0.1;
+    0.2 0.3 0.5;
+    0.3 0.3 0.4])'
 %% alinea b
 X=[1;0;0];
 X2 = T^2*X
-n=[1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20];
-y = 
+%% alinea c
+n=(1:20);
+
+for i = 1:20
+    transitions = T^i;
+    plotsol(i) = transitions(1);
+    plotnuvens(i) = transitions(2);
+    plotchuva(i) = transitions(3);
+end
+subplot(1,2,1)
+title("Alinea C")
+plot(n,plotsol);
+hold on
+plot(n,plotnuvens);
+plot(n,plotchuva);
+hold off
+%% alinea d
+n=(1:20);
+i=1;
+while(true)
+    transitions = T^i;
+    plotsol(i) = transitions(1);
+    plotnuvens(i) = transitions(2);
+    plotchuva(i) = transitions(3);
+    if(i>1)
+       if( abs(plotsol(i)- plotsol(i-1) )< 10^-4)
+           break;
+       end
+       if( abs(plotnuvens(i)- plotnuvens(i-1)) < 10^-4)
+           break;
+       end
+       if( abs(plotchuva(i)- plotchuva(i-1)) < 10^-4)
+           break;
+       end
+    end
+   i=i+1;
+end
+subplot(1,2,2)
+title("Alinea D")
+plot(n,plotsol);
+hold on
+plot(n,plotnuvens);
+plot(n,plotchuva);
+hold off
+
+
---
+++ Pratica8_Hugi/untitled.m
@@ -0,0 +1,1 @@
+A = xlsread('Livro1.xlsx', '')
