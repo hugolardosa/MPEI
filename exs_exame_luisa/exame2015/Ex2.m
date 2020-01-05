2b4ac51ab03e6b62f4924dd462c0cc91b00f82f6
LuisaTheAmaral <luisa.amaral9@gmail.com>
2020-01-05 19:37:26 +0000

    exames resolvidos

--- exs_exame_luisa/exame2015/Ex2.m
+++ exs_exame_luisa/exame2015/Ex2.m
@@ -1,28 +1,54 @@
 %% Exercício 2
 clc
 clear
-%% alínea a
-% matriz de hyperlinks (necessita de estar na forma canónica?)
-H = [0   1/2 1/3 1/4 0;
-     1/2 0   0   1/4 1/2;
-     1/2 1/2 1/3 1/4 0;
-     0   0   0   0   1/2;
-     0   0   1/3 1/4 0];
+%% alínea a 
+
+T = [0    0.1 0   0.5;
+     0.1  0   0   0.1;
+     0.05 0.1 0   0.1;
+     0.01 0.1 0.1 0];
  
- %[1/N], com N = 5 pois há 5 páginas
- N = ones(5,5)/5;
+ v = ([1 2 10 5]*10e6)';
  
- B = 0.8;
- % matriz da Google
- A = B*H + (1-B)*N
+ Tr = T^7*v;
+ fprintf('Angola: %f toneladas', Tr(1))
+ fprintf('\nBrasil: %f toneladas', Tr(2))
+ fprintf('\nChile: %f toneladas', Tr(3))
+ fprintf('\nDinamarca: %f toneladas\n', Tr(4))
  
  %% alínea b
+ A = 0;
+ B = 0;
+ C = 0;
+ D = 0;
+ i = 1;
+ while(true)
+     atual = T^i*v;
+     A = [A atual(1)];
+     B = [B atual(2)];
+     C = [C atual(3)];
+     D = [D atual(4)];
+     if(i > 1) 
+         anterior = T^(i-1)*v;
+         if(abs(atual - anterior) < 0.01)
+             break;
+         end
+     end
+     i = i + 1;
+ end
+ fprintf('\nAngola: %.1f toneladas', max(A))
+ fprintf('\nBrasil: %.1f toneladas', max(B))
+ fprintf('\nChile: %.1f toneladas', max(C))
+ fprintf('\nDinamarca: %.1f toneladas\n', max(D))
  
- v = ones(5,1)/5;
- Pr = A^10*v;
- fprintf('\nC = %f', Pr(1))
- fprintf('\nD = %f', Pr(2))
- fprintf('\nE = %f', Pr(3))
- fprintf('\nF = %f', Pr(4))
- fprintf('\nG = %f\n', Pr(5))
+ %% alínea c
+ j = 1;
+ while(true)
+     q = T^j*v;
+     if(q(4) < 2*10e6)
+         break;
+     end
+     j = j + 1;
+ end
  
+ fprintf('\nMês %d\n', j)
